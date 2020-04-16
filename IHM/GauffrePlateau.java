8a3c0c3f2f18d13247ba9ed0b05aaa7850228435
scartelo <58155534+scartelo@users.noreply.github.com>
2020-04-16 07:36:53 +0200

    Add files via upload
    Améliorations IHM

--- IHM/GauffrePlateau.java
+++ IHM/GauffrePlateau.java
@@ -1,52 +1,146 @@
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.InputStream;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.io.IOException;
 import java.io.File;
 
-public class GauffrePlateau extends JComponent {
-	private Image poison,gaufre;
-	
-	public GauffrePlateau () {
-			poison = lisImage ("poison.jpg");
-			gaufre = lisImage ("gauffre_2.png");
+public class GauffrePlateau extends JPanel implements MouseListener{
+	private Image poison, gaufre;
+	private int nbLignes , nbColonnes ;
+	private int largeurCase = 100;
+
+	public GauffrePlateau(int l, int c) {
+		poison = lisImage("poison.jpg");
+		gaufre = lisImage("gauffre_2.png");
+		this.nbLignes = l;
+		this.nbColonnes = c;
 	}
-	private Image lisImage (String nom) {
+	
+	
+
+	public int getLargeurCase() {
+		return largeurCase;
+	}
+
+
+
+	public void setLargeurCase(int largeurCase) {
+		this.largeurCase = largeurCase;
+	}
+
+
+
+	public int getNbLignes() {
+		return nbLignes;
+	}
+
+
+
+	public void setNbLignes(int nbLignes) {
+		this.nbLignes = nbLignes;
+	}
+
+
+
+	public int getNbColonnes() {
+		return nbColonnes;
+	}
+
+
+
+	public void setNbColonnes(int nbColonnes) {
+		this.nbColonnes = nbColonnes;
+	}
+
+
+
+	private Image lisImage(String nom) {
 		Image img = null;
 		try {
-			img = ImageIO.read(new File("Images/"+nom));
+			img = ImageIO.read(new File("IHM/Images/" + nom));
 		} catch (IOException io) {
-			System.out.println ("Chargment de l'image "+nom+" Impossible");
-			System.exit (1);
+			System.out.println("Chargment de l'image " + nom + " Impossible");
+			System.exit(1);
 		}
 		return img;
 	}
+
 	private void tracer(Graphics2D g2D, Image i, int x, int y, int l, int h) {
-		g2D.drawImage(i, x, y, l, h, null);
+		g2D.drawImage(i, x, y, l, h, this);
 	}
-	public void paintComponent (Graphics g) {
-		Graphics2D g2D = (Graphics2D)g;
-		int l = 10,c = 10;
-		int largeur = getSize ().width;
-		int hauteur = getSize ().height;
-		int largeurCase = largeur / c;
-		int hauteurCase = hauteur / l;
+
+	public void paintComponent(Graphics g) {
+		Graphics2D g2D = (Graphics2D) g;
 		
-		largeurCase = hauteurCase =  Math.min (largeurCase,hauteurCase);
-		g2D.clearRect (0,0,largeur,hauteur);
-		
-		for (int i = 0; i < l; i ++) {
-			for (int j = 0; j < c; j ++) {
-				int x,y;
-				x = j * largeurCase;
-				y = i * hauteurCase;
+		/*int largeur = getSize().width;
+		System.out.println("largeur = " + largeur);
+		int hauteur = getSize().height;
+		System.out.println("hauteur " + hauteur);
+		int largeurCase = largeur / nbLignes;
+		System.out.println("largeur case = " + largeurCase);
+		int hauteurCase = hauteur / nbLignes;
+		System.out.println("hauteur case = " + hauteurCase);
+
+
+		largeurCase = hauteurCase = Math.min(largeurCase, hauteurCase);
+		System.out.println("largeur hauteur case = " + hauteurCase);
+		//g2D.clearRect(0, 0, largeur, hauteur);*/
+
+		for (int j = 0; j < this.nbColonnes; j++) {
+			for (int i = 0; i < this.nbLignes; i++) {
+				int x, y;
+				y = j * largeurCase;
+				x = i * largeurCase;
+				
 				if (i == 0 && j == 0) {
-					tracer (g2D,poison,x,y,largeurCase,hauteurCase);
+					tracer(g2D, poison, x, y, largeurCase, largeurCase);
 				} else {
-					tracer (g2D,gaufre,x,y,largeurCase,hauteurCase);
+					tracer(g2D, gaufre, x, y, largeurCase, largeurCase);
 				}
 			}
 		}
+	}
+
+
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
--- IHM/InterfaceGraphique.java
+++ IHM/InterfaceGraphique.java
@@ -1,56 +1,50 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class InterfaceGraphique implements Runnable {
-	JFrame frame;
-	JPanel container = new JPanel ();
-	GauffrePlateau gaufP;
-	public InterfaceGraphique () {
+public class InterfaceGraphique extends JFrame implements Runnable {
+
+	JPanel container = new JPanel();
+	JPanel boutonsPanel = new JPanel();
+	GauffrePlateau gaufP = new GauffrePlateau(8, 6);
+
+	public InterfaceGraphique() {
+		this.setTitle("Gauffres Empoisonnées");
+
+		JButton IA = new JButton("IA");
+		JButton suivant = new JButton(">");
+		JButton precedent = new JButton("<");
+		JButton hist = new JButton("historique");
+		JButton sauv = new JButton("save");
+		JButton annuler = new JButton("reset");
+
+		boutonsPanel.setLayout(new FlowLayout());
+		boutonsPanel.add(annuler);
+		boutonsPanel.add(sauv);
+		boutonsPanel.add(precedent);
+		boutonsPanel.add(suivant);
+		boutonsPanel.add(hist);
+		boutonsPanel.add(IA);
+
+		container.setLayout(new BorderLayout());
+		container.add(boutonsPanel, BorderLayout.NORTH);
+		container.add(gaufP);
+		int lFenetre = gaufP.getLargeurCase() * gaufP.getNbLignes();
+		System.out.println("lfenetre " + lFenetre);
+		int hFenetre = (gaufP.getLargeurCase() * gaufP.getNbColonnes()) + 65;
+		this.setSize(lFenetre, hFenetre);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.setContentPane(container);
+		this.setVisible(true);
+		run();
+
 	}
-	public void demarrer () {
+
+	public void demarrer() {
 		SwingUtilities.invokeLater(new InterfaceGraphique());
 	}
-	public void run () {
-		frame = new JFrame ("Gauffres Empoisonnées");
-		gaufP = new GauffrePlateau ();
-		Box boiteTexte = Box.createVerticalBox ();
-		
-		
-		frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-		frame.setSize (750,540);
-		frame.setLocationRelativeTo (null);
-		 
-		JButton IA = new JButton ("IA");
-		JButton suivant = new JButton (">");
-		JButton precedent = new JButton ("<");
-		JButton hist = new JButton ("historique");
-		JButton sauv = new JButton ("save");
-		JButton annuler = new JButton ("reset");
-		JLabel label = new JLabel ("Gaufre Empoisonnée");
-		
-		
-		
-		Box s_r = Box.createHorizontalBox ();
-		s_r.add (sauv);
-		s_r.add (annuler);
-		Box n_p = Box.createHorizontalBox ();
-		n_p.add (precedent);
-		n_p.add (suivant);
-		
-		label.setAlignmentX (Component.CENTER_ALIGNMENT);
-		boiteTexte.add (label);
-		//boiteTexte.add (Box.createGlue ());
-		IA.setAlignmentX (Component.CENTER_ALIGNMENT);
-		boiteTexte.add (IA);
-		hist.setAlignmentX (Component.CENTER_ALIGNMENT);
-		boiteTexte.add (hist);
-		s_r.setAlignmentX (Component.CENTER_ALIGNMENT);
-		boiteTexte.add (s_r);
-		n_p.setAlignmentX (Component.CENTER_ALIGNMENT);
-		boiteTexte.add (n_p);
-		
-		frame.add (boiteTexte,BorderLayout.EAST);
-		frame.add (gaufP);
-		frame.setVisible (true);
+
+	public void run() {
+
 	}
 }
